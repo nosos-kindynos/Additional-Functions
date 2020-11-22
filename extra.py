6d400e86b761c06734bcc505852c1b057fd7fa95
nosos-kindynos <73609672+nosos-kindynos@users.noreply.github.com>
2020-11-23 01:30:56 +0530

    Create extra.py

---
+++ extra.py
@@ -0,0 +1,14 @@
+def estimate_time(processer_frequency):
+    from functools import reduce
+    n=len(paths)
+    def ncr(n, r):
+        r = min(r, n-r)
+        numer = reduce(op.mul, range(n, n-r, -1), 1)
+        denom = reduce(op.mul, range(1, r+1), 1)
+        return numer // denom  # or / in Python 2
+    s=0
+    p=3
+    for i in range(1, n+1):
+        s += i*p*(ncr(n, i))
+        k = s/(processer_frequency)
+    return(k)
